fba482a92867e2da9c367d88a9affb4593d2a761
Meaghan Searle <msearle88@gmail.com>
2019-01-21 19:37:41 -0800

    Basic Movement

---
+++ DGM2610Project/Assets/MovePatternChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class MovePatternChange : ScriptableObject
+{
+    
+    	public MovePattern MovePatternTransfer;
+    
+    
+    
+    	public MovePattern Transfer()
+    
+    	{
+    
+    		return MovePatternTransfer;
+    
+    	}
+}
---
+++ DGM2610Project/Assets/TransferTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransferTrigger : MonoBehaviour
+{
+    public MovePatternChange PatternChange;
+
+    public GameObject other;
+
+
+
+    private void OnTriggerEnter(Collider other)
+
+
+
+    {
+
+
+        other.GetComponent<MoveBase>().MovePattern = PatternChange.Transfer ();
+
+
+
+
+    }
+}
