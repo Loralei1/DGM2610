6963a4bfae249744eb9ef2c018f7159693d659df
Meaghan Searle <msearle88@gmail.com>
2019-01-28 23:01:15 -0800

    movement events

---
+++ DGM2610Project/Assets/DoubleJumpChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJumpChange : MonoBehaviour
+{
+    public bool CanDouble;
+    public MovePatternChange ToDoubleJump;
+
+    private void Start()
+    {
+        CanDouble = true;
+    }
+
+    private void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ DGM2610Project/Assets/OnKeyEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnKeyEvent : MonoBehaviour
+{
+    public UnityEvent Event;
+    public UnityEvent SecondEvent;
+    
+
+    private void Update()
+    {
+       
+        if (Input.GetButtonDown("Run") == true)
+        {
+            Event.Invoke();
+        }
+
+        else
+        {
+         SecondEvent.Invoke();   
+        }
+    }
+}
