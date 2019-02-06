7a04e750d090aa1c964a78cad297f7c087a37007
Meaghan Searle <msearle88@gmail.com>
2019-02-06 12:51:14 -0800

    Lamps and fall blocks

---
+++ DGM2610Project/Assets/CountDownDisplay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownDisplay : MonoBehaviour
+{
+    public FloatData time;
+
+    public Text Countdown;
+
+
+
+    private void Start()
+
+    {
+
+        Countdown.text = "Time: " + time.value.ToString();
+
+    }
+
+
+
+    private void SetScoreText()
+
+    {
+
+        Countdown.text = "Time: " + time.value.ToString();
+
+    }
+
+
+
+    private void Update()
+
+    {
+
+        SetScoreText();
+
+    }
+}
