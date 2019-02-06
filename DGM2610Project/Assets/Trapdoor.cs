89ce7bb5c8286dc4bde0b50339b963c7a790b6a4
Meaghan Searle <msearle88@gmail.com>
2019-02-06 11:19:51 -0800

    Level fixes

---
+++ DGM2610Project/Assets/Trapdoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trapdoor : MonoBehaviour
+
+
+{
+
+    public GameObject trapdoor; 
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(trapdoor);
+        }
+    }
+}
