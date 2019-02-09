ae797203c0a98bd84c1be5cd7b7c0aecf2098908
Meaghan Searle <msearle88@gmail.com>
2019-02-09 14:59:34 -0800

    sluagh

---
+++ DGM2610Project/Assets/FlockFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockFollow : MonoBehaviour
+{
+    public Transform target;
+    private float speed; 
+
+    private void Update()
+    {
+        speed = Random.Range(12f, 16f);
+        Vector3 localPosition = target.transform.position - transform.position;
+        localPosition = localPosition.normalized;
+        transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z);
+        transform.LookAt(target.position);
+    }
+}
