9dd395ad48f743bd5ec96aa1d48f7c95b1cb6971
Meaghan Searle <msearle88@gmail.com>
2019-02-10 15:59:02 -0800

    sluagh kind of working

--- DGM2610Project/Assets/FlockFollow.cs
+++ DGM2610Project/Assets/FlockFollow.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class FlockFollow : MonoBehaviour
 {
     public Transform target;
-    private float speed; 
+    private float speed;
+    private float distance = 2f;
+    public GameObject[] otherSluagh;
 
     private void Update()
     {
-        speed = Random.Range(12f, 16f);
+        speed = Random.Range(10f, 16f);
+        Vector3 localRotation = target.position;
         Vector3 localPosition = target.transform.position - transform.position;
+        transform.position = (transform.position - target.transform.position).normalized * distance + target.transform.position; 
         localPosition = localPosition.normalized;
         transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z);
-        transform.LookAt(target.position);
+       
     }
 }
