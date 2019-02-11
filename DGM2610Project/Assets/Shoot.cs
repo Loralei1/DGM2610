574de8777fd422d162894ba8662b1dff41c00bfc
Meaghan Searle <msearle88@gmail.com>
2019-02-10 20:53:03 -0800

    Shooting

---
+++ DGM2610Project/Assets/Shoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+		public GameObject Shot;
+    
+    	public Transform shotSpot;
+    
+    	public float fireRate;
+    
+    
+    
+    	private float nextFire; 
+    
+    
+    
+    	private void Update()
+    
+    	{
+    
+    		if (Input.GetButton("Fire1") && Time.time > nextFire)
+    
+    		{
+    
+    			nextFire = Time.time + fireRate; 
+    
+    			Instantiate(Shot, shotSpot.position, shotSpot.rotation);
+    
+    		}
+    
+    	} 
+}
---
+++ DGM2610Project/Assets/ShotMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotMove : MonoBehaviour
+{
+    private Rigidbody rb; 
+
+    public float Speed; 
+
+
+
+    void Start ()
+
+
+
+    {
+
+
+
+        rb = GetComponent<Rigidbody> ();
+
+        rb.velocity = transform.right * Speed; 
+
+
+
+    }
+
+
+}
