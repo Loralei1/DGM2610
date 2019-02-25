162b0aaf5c1d8b9dc3580d502ebb513f58b5cf2e
Meaghan Searle <msearle88@gmail.com>
2019-02-25 01:04:39 -0800

    save before recording

---
+++ DGM2610Project/Assets/SplashTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SplashTrigger : MonoBehaviour
+{
+    public UnityEvent Event;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Event.Invoke();
+        }
+        
+    }
+}
