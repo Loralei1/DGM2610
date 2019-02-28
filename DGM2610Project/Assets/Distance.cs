f469b43755af4ed19dc9a850ba05bdb902db6c7c
Meaghan Searle <msearle88@gmail.com>
2019-02-28 14:40:23 -0700

    trying to get light/enemy interaction to work

---
+++ DGM2610Project/Assets/Distance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distance : MonoBehaviour
+{
+    public FloatData dist; 
+    
+    
+    void Start(Collider other)
+    {
+        if (other.gameObject.CompareTag("ForceField"))
+        {
+            var dist = 2; 
+        }   
+    }
+
+   
+}
