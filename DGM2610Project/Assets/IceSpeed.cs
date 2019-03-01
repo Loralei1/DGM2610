fbccc8a78545ba223fe9dd52895deb016d684c21
Meaghan Searle <msearle88@gmail.com>
2019-02-28 21:41:12 -0800

    Icebox working

---
+++ DGM2610Project/Assets/IceSpeed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceSpeed : MonoBehaviour
+{
+    public FloatData speed;
+
+    private void Start()
+    {
+        gameObject.active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (gameObject.active = true)
+        {
+            speed.value = 0;
+            break; 
+        }
+    }
+}
---
+++ DGM2610Project/Assets/Icebox.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public class Icebox : MonoBehaviour
+{
+    public UnityEvent On;
+    public UnityEvent Off;
+    
+     
+    
+    
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            
+        {
+            Debug.Log("Working");
+            On.Invoke();
+            yield return new WaitForSeconds(3);
+            Off.Invoke();
+
+        }
+    }
+    
+
+    
+}
