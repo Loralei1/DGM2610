d4d2e237c40204acb1fef789d0a3d09be82979bc
Meaghan Searle <msearle88@gmail.com>
2019-02-28 20:37:20 -0800

    enemy distance script

--- DGM2610Project/Assets/Distance.cs
+++ DGM2610Project/Assets/Distance.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class Distance : MonoBehaviour
 {
-    public FloatData dist; 
-    
-    
-    void Start(Collider other)
-    {
-        if (other.gameObject.CompareTag("ForceField"))
-        {
-            var dist = 2; 
-        }   
-    }
 
-   
+    public GameObject other; 
+    public FloatData distance;
+    public Vector3 pos;
+
+    private void Update()
+    {
+        distance.value = Vector3.Distance(this.transform.position, other.transform.position);
+
+        if (distance.value > 25)
+        {
+            Debug.Log(distance);
+            distance.value = 25;
+            transform.position = (transform.position - other.transform.position).normalized * distance.value +
+                                 other.transform.position;
+        }
+
+        //os = other.transform.position;
+        //transform.RotateAround(pos, Vector3.up, 20 * Time.deltaTime);
+    }
 }
