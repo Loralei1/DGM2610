674d50b2b549096595d0f9475dc6bfc2937a278f
Meaghan Searle <msearle88@gmail.com>
2019-03-04 23:19:42 -0800

    save after recording

---
+++ DGM2610Project/Assets/FireList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireList : MonoBehaviour
+{
+
+    public List<GameObject> Fireflies;
+
+   
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Fireflies.Count >= 1)
+        {
+            Fireflies[1].SetActive(false);
+            
+        }
+    }
+}
