4e03ed8182c7886797293c932d19aa76af739ebc
Meaghan Searle <msearle88@gmail.com>
2019-03-13 10:34:58 -0700

    importing the meshes and materials

--- DGM2610Project/Assets/EnemyStayAway.cs
+++ DGM2610Project/Assets/EnemyStayAway.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class EnemyStayAway : MonoBehaviour
 {
-        public GameObject other; 
-        public FloatData distance;
-        
-    
-        private void Update()
-        {
-            distance.value = Vector3.Distance(this.transform.position, other.transform.position);
-    
-            if (distance.value <= 10)
-            {
-                Debug.Log(distance);
-                distance.value = 11;
-                transform.position = (transform.position - other.transform.position).normalized * distance.value +
-                                     other.transform.position;
-            }
+    public GameObject other;
+    public FloatData distance;
 
-        
+
+    private void Update()
+    {
+        distance.value = Vector3.Distance(this.transform.position, other.transform.position);
+
+        if (distance.value <= 10)
+        {
+            Debug.Log(distance);
+            distance.value = 11;
+            transform.position = (transform.position - other.transform.position).normalized * distance.value +
+                                 other.transform.position;
+        }
+
+
+    }
 }
