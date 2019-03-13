cb2d3ccd5ca7ef1428a7fa98ffda3ff481a9f531
Meaghan Searle <msearle88@gmail.com>
2019-03-12 20:57:22 -0700

    enemy stay away script

--- DGM2610Project/Assets/EnemyStayAway.cs
+++ DGM2610Project/Assets/EnemyStayAway.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class EnemyStayAway : MonoBehaviour
 {
+        public GameObject other; 
+        public FloatData distance;
+        
     
+        private void Update()
+        {
+            distance.value = Vector3.Distance(this.transform.position, other.transform.position);
+    
+            if (distance.value <= 10)
+            {
+                Debug.Log(distance);
+                distance.value = 11;
+                transform.position = (transform.position - other.transform.position).normalized * distance.value +
+                                     other.transform.position;
+            }
+
+        
 }
