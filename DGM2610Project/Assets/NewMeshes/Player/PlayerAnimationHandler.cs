c4ed5c32fb545af2510586535babbda52f2d110f
Meaghan Searle <msearle88@gmail.com>
2019-04-18 11:10:49 -0600

    climb, run, and swim working

--- DGM2610Project/Assets/NewMeshes/Player/PlayerAnimationHandler.cs
+++ DGM2610Project/Assets/NewMeshes/Player/PlayerAnimationHandler.cs
@@ -1,21 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 
 public class PlayerAnimationHandler : MonoBehaviour
 {
 
-    public Animator playerAnimatorController; 
-    
+    public Animator playerAnimatorController;
+    public CharacterController controller; 
+
+    private void Update()
+    {
+
+        if (Input.GetButton("Horizontal") && controller.isGrounded)
+        {
+            playerAnimatorController.SetBool("ToRun", true);
+            playerAnimatorController.SetBool("FromRun", false);
+            playerAnimatorController.SetBool("ToSwim", false);
+           
+           
+        }
+
+       /* if (Input.GetButton("Horizontal") == false && !controller.isGrounded)
+        {
+            playerAnimatorController.SetBool("FromRun", true);   
+        }
+        */
+        
+
+        if (Input.GetButton("Jump"))
+        {
+            playerAnimatorController.SetBool("ToJump",true);
+        }
+
+        if (Input.GetButton("Jump") == false && Input.GetButton("Horizontal") == false && !controller.isGrounded)
+        {
+            playerAnimatorController.SetBool("ToFall",true);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Ladder"))
+        if (other.gameObject.CompareTag("Ladder") && Input.GetButton("Vertical"))
         {
+            Debug.Log("Climb");
+            playerAnimatorController.SetBool("FromClimb", false);
             playerAnimatorController.SetBool("ToClimb", true);
         }
 
         if (other.gameObject.CompareTag("Water"))
         {
+            playerAnimatorController.SetBool("FromSwim", false);
             playerAnimatorController.SetBool("ToSwim", true);
         }
     }
@@ -25,12 +60,24 @@ public class PlayerAnimationHandler : MonoBehaviour
         if (other.gameObject.CompareTag("Ladder"))
         {
             playerAnimatorController.SetBool("FromClimb", true);
+            playerAnimatorController.SetBool("ToClimb", false);
         }
         
         if (other.gameObject.CompareTag("Water"))
         {
-            playerAnimatorController.SetBool("ToSwim", true);
+            playerAnimatorController.SetBool("ToSwim", false);
+            playerAnimatorController.SetBool("FromSwim", true);
         }
     }
-   
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+        if (Input.GetButton("Horizontal") && controller.isGrounded)
+        {
+            playerAnimatorController.SetBool("ToLand", true);
+        }
+           
+       
+    }
 }
