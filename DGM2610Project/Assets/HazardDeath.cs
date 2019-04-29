b23e00a5ee4952ada70763c1217fcbcde1bfc674
Meaghan Searle <msearle88@gmail.com>
2019-04-29 11:12:24 -0700

    save before recording

--- DGM2610Project/Assets/HazardDeath.cs
+++ DGM2610Project/Assets/HazardDeath.cs
@@ -8,11 +8,11 @@ public class HazardDeath : MonoBehaviour
     public FloatData drowningTime;
     public FloatData playerHealth;
 
-    IEnumerator Update()
+    IEnumerator Start()
     {
         if (drowningTime.value == 0 || playerHealth.value <= 0)
         {
-           yield return new WaitForSeconds(3);
+           yield return new WaitForSeconds(1.5f);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
