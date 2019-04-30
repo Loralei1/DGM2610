769f3ba39a0c089ece7ad5e20ac65755ad45ac16
Meaghan Searle <msearle88@gmail.com>
2019-04-30 09:16:35 -0600

    fixing thorn sound

---
+++ DGM2610Project/Assets/RunSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class RunSound : MonoBehaviour
+{
+    public AudioSource run;
+
+    private void Start()
+    {
+        run = GetComponent<AudioSource>();
+       
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            run.Play();
+        }
+    }
+}
