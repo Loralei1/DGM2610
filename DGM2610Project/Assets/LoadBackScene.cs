b2b9e8f3b8e2cbee5e5295c2f5c57c21c70225a9
Meaghan Searle <msearle88@gmail.com>
2019-04-30 22:18:09 -0700

    LastTweaks

---
+++ DGM2610Project/Assets/LoadBackScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadBackScene : MonoBehaviour
+{
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
