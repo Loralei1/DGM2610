260b1b3ae73ab12db7d13f61ffabc70c78232413
Meaghan Searle <msearle88@gmail.com>
2019-05-02 21:13:40 -0700

    z transform fix

---
+++ DGM2610Project/Assets/Scripts/ZPos.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZPos : MonoBehaviour
+{
+    public Transform transform;
+
+    private void Update()
+    {
+        if (transform.position.z > 0 || transform.position.z < 0)
+        {
+            var transformPosition = transform.position;
+            transformPosition.z = 0;
+        }
+    }
+}
