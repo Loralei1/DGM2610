daf2f65a73932801ef79f9721575014309ed4efe
Meaghan Searle <msearle88@gmail.com>
2019-05-02 21:03:35 -0700

    organization

---
+++ DGM2610Project/Assets/Scripts/HazInstant.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazInstant : MonoBehaviour
+{
+    public GameObject haz;
+
+    public Transform hazSpot;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Instantiate(haz, hazSpot.position, hazSpot.rotation);
+        }
+       
+    }
+}
---
+++ DGM2610Project/Assets/Scripts/HazardDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HazardDeath : MonoBehaviour
+{
+    public FloatData drowningTime;
+    public FloatData playerHealth;
+
+    IEnumerator Start()
+    {
+        if (drowningTime.value == 0 || playerHealth.value <= 0)
+        {
+           yield return new WaitForSeconds(1.5f);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
